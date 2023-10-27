7c7cc8eb09a298a9246b1373d6ce0655c54e3160
Muhammad Shamin Farhan <140752813+farhanparru@users.noreply.github.com>
2023-10-27 01:28:24 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import WeekFetch from './API FETCH/API TASK/WeekFetch';
+
+
+function App() {
+    return (
+        <div className="App">
+       <WeekFetch/>
+        </div>
+    );
+}
+
+export default App;
